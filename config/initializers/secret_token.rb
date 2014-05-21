5b3c63c51c6289c6cfcc6304dedeed5be910fb34
Sebastián Puente <sebas7pr@gmail.com>
2014-05-21 03:15:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fair2::Application.config.secret_key_base = 'b8ed7c38940e44b2bcd90068d3e70bf53c8dd2516558c17ea5ba42db248be4a1fcb5f766a7a0806acf394259bbc29153d257555add46d3e5e8a135a39f35b05d'
